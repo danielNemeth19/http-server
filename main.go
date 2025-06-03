f0c8573ac24212a69cfaaf2c2db66993e48aa39a
Daniel Nemeth <daniel.nemeth83@yahoo.com>
2025-06-04 00:56:58 +0200

    (feat) Readiness and basic file handler

--- main.go
+++ main.go
@@ -4,9 +4,16 @@ import (
 	"net/http"
 )
 
+func myHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+}
+
 func main() {
 	serverMux := http.NewServeMux()
-	serverMux.Handle("/", http.FileServer(http.Dir('.')))
+	serverMux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	serverMux.HandleFunc("/healthz", myHandler)
 	server := http.Server{Handler: serverMux, Addr: ":8080"}
 	server.ListenAndServe()
 }
